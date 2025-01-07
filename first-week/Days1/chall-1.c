4a700247ccd8b7889a25c481ec429d5350c8d06f
mouad <mouadsertati9@gmail.com>
2025-01-07 16:03:13 +0100

    first day

---
+++ first-week/Days1/chall-1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int n, i, sum;
+    printf("enter une numbre : ");
+    scanf("%d", &n);
+    sum = 0;
+
+    for (i = 0; i <= n; i++)
+        sum = sum + pow(10, i);
+
+    printf("la somme est = %d", sum);
+}
